b9600fc437c9ac643b31181d434b9ff4f1c6cfba
Joy Zhang <joyzhang0713@gmail.com>
2025-02-10 23:10:17 -0500

    before docker

--- app/main.py
+++ app/main.py
@@ -1,40 +1,40 @@
 import csv
-import glob
 import os
+from sentence_transformers import SentenceTransformer
+import json
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
-def multiply_by_two(x):
-    return int(x) * 2
+def get_database(database_embedding):
+    with open(database_embedding, "r", encoding="utf-8") as f:
+        database_eb = json.load(f)
+    return database_eb
 
-def read_csv(input_file):
-    rows = []
-    with open(input_file, mode='r') as infile:
-        reader = csv.reader(infile)
+def write_csv(output_file, top_3):
+    with open(output_file, mode='w', encoding='utf-8') as outfile:
+        json.dump(top_3, outfile, ensure_ascii=False, indent=4)
 
-        for row in reader:
-            multiplied_row = [multiply_by_two(value) for value in row]
-            rows.append(multiplied_row)
+def calculate_sim(model, database_eb, user_input):
+    user_eb = model.encode(user_input)
+    similarity = {}
 
-    return rows
-
-def write_csv(output_file, rows):
-    with open(output_file, mode='w', newline='') as outfile:
-        writer = csv.writer(outfile)
-        writer.writerows(rows)
-
-def multiply_csvs(input_directory, output_directory):
-    csv_files = glob.glob(os.path.join(input_directory, '*.csv'), recursive=False)
-    for input_file in csv_files:
-        filename = os.path.basename(input_file)
-        output_filename = 'multiplied_' + filename
-        output_file = os.path.join(output_directory, output_filename)
-
-        rows = read_csv(input_file)
-        write_csv(output_file, rows)
+    for disease, eb in database_eb.items():
+        similarity[disease] = cosine_similarity(user_eb.reshape(1,-1), np.array(eb).reshape(1,-1))
+    top_3 = sorted(similarity.items(), key=lambda item: item[1], reverse=True)[:3]
+    top_3_final = [(key, value[0][0]) for key, value in top_3]
+    return top_3_final
 
 if __name__ == "__main__":
+    user_input = ["cough, fever, and sour throat"]
+    model = SentenceTransformer('sentence-transformers/paraphrase-MiniLM-L6-v2')
+    database_embedding = r"D:\Users\Joy\Desktop\Joy\data\input\disease_embedding.json"
+    database_eb = get_database(database_embedding)
+
     base_directory = os.path.dirname(os.path.dirname(__file__))
 
-    input_directory = os.getenv('INPUT_DIR', os.path.join(base_directory, 'data/input/'))
+    # input_directory = os.getenv('INPUT_DIR', os.path.join(base_directory, 'data/input/'))
     output_directory = os.getenv('OUTPUT_DIR', os.path.join(base_directory, 'data/output/'))
+    output_file = os.path.join(output_directory, 'result.json')
 
-    multiply_csvs(input_directory, output_directory)
+    top_3 = calculate_sim(model, database_eb, user_input)
+    write_csv(output_file, top_3)
